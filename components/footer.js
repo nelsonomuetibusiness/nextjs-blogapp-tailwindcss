1d67a04d7843548f84c0675a164332c64b6d8e55
nelsonomuetibusiness <nelson.omueti.business@gmail.com>
2023-09-04 17:46:03 +0100

    trending section of blog

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,14 @@
+import r3 from "@/public/images/r3.jpg"
+import Image from "next/image"
+
+
+
 export default function footer() {
   return (
-    <div className="text-3xl">this is footer</div>
+    <div className="text-3xl">
+    
+
+    <Image  src={r3} alt="" width={400} height={400}  />
+    </div>
   )
 }
--- components/section1.js
+++ components/section1.js
@@ -1,7 +1,23 @@
-
+import Image from "next/image";
+import r3 from "@/public/images/r3.jpg"
 
 export default function section1() {
   return (
-    <div>section1</div>
-  )
+    <section className="py-16">
+      <div className="container mx-auto md:px-20">
+        <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
+      </div>
+    </section>
+  );
+}
+
+function Slide() {
+  return (
+    <div className="grid md:grid-cols-2">
+      <div className="image">
+      <Image  src={r3} alt="" width={400} height={400}  />
+      </div>
+      <div className="info"></div>
+    </div>
+  );
 }
